d30da8f7eb57169742f0d418f68316afdc5164dd
JoelPlourde <joelplourde2@gmail.com>
2021-03-28 14:01:05 -0400

    Added Tooltip

---
+++ Assets/Scripts/System/Tooltip/Tooltip.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+	[RequireComponent(typeof(Canvas))]
+	public class Tooltip : MonoBehaviour {
+		private RectTransform _rectTransform;
+		private RectTransform _childrenRectTransform;
+		public ContentSizeFitter _contentSizeFitter;
+		private Canvas _canvas;
+		private Text _text;
+
+		private Vector3 _offset = new Vector2(-20, 20);
+		private Vector2 _padding = new Vector2(20, 10);
+
+		private void Awake() {
+			if (Instance == null) {
+				Instance = this;
+			}
+
+			_canvas = GetComponent<Canvas>();
+			if (_canvas == null) {
+				throw new UnityException("Canvas component is null.");
+			}
+
+			_text = GetComponentInChildren<Text>();
+			if (_text == null) {
+				throw new UnityException("Text component is null.");
+			}
+
+			_rectTransform = GetComponent<RectTransform>();
+			_childrenRectTransform = transform.GetChild(0).GetComponent<RectTransform>();
+
+			_canvas.enabled = false;
+		}
+
+		public void ShowTooltip(string text) {
+			transform.position = Input.mousePosition - _offset;
+			_text.text = text;
+			StartCoroutine(UpdateContentSize());
+		}
+
+		public void HideTooltip() {
+			CancelInvoke();
+			_canvas.enabled = false;
+			CancelInvoke();
+		}
+
+		private void FollowMouse() {
+			transform.position = Input.mousePosition - _offset;
+		}
+
+		private IEnumerator UpdateContentSize() {
+			yield return new WaitForEndOfFrame();
+			_rectTransform.sizeDelta = _childrenRectTransform.sizeDelta + _padding;
+			InvokeRepeating(nameof(FollowMouse), 0f, 0.016f);
+			_canvas.enabled = true;
+		}
+
+		public static Tooltip Instance { get; private set; }
+	}
+}
