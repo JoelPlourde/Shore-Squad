15ccb51203fa43984f012dda9170c78fc3db6758
JoelPlourde <joelplourde2@gmail.com>
2021-04-13 08:20:34 -0400

    Separated NPC from the Actor component

--- Assets/Scripts/Humanoid/NPC.cs
+++ Assets/Scripts/Humanoid/NPC.cs
@@ -1,38 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using DialogueSystem;
 using TaskSystem;
 
-public class NPC : Actor, IInteractable
+public class NPC : MonoBehaviour, IInteractable
 {
+	[Tooltip("The identifier for this NPC, this is how the dialogue will be instantiated by.")]
+	public string Identifier;
+
+	[Tooltip("The current dialogue when the player interacts with this NPC.")]
 	public DialogueData DialogueData;
+
 	private Actor _actor;
 
-	public override void Start() {
-		base.Start();
+	public void Start() {
 
-		Playable = false;
+		if (string.IsNullOrWhiteSpace(Identifier)) {
+			throw new UnityException("Please provide the Identifier for this NPC: " + transform.name);
+		}
 
-		if (!ReferenceEquals(DialogueData, null)) {
-			UserInputs.Instance.Subscribe(Guid.ToString(), Interact);
-		} else {
+		if (ReferenceEquals(DialogueData, null)) {
 			throw new UnityException("Please assign a DialogueData unto this NPC!");
 		}
+
+		transform.name = Identifier;
+
+		UserInputs.Instance.Subscribe(Identifier, Interact);
 	}
 
 	public void Interact(MouseButton mouseButton, RaycastHit raycastHit) {
 		if (Squad.First(out Actor actor)) {
 			_actor = actor;
 			InteractArguments interactArguments = new InteractArguments(1f, this.transform.position, this);
-			_actor.TaskScheduler.CreateTask<Interact>(interactArguments, TaskPriority.LOW);
+			_actor.TaskScheduler.CreateTask<Interact>(interactArguments);
 		}
 	}
 
 	public void OnInteractEnter() {
 		Debug.Log("Start interaction !");
 
-		CameraSystem.CameraTarget.Instance.ZoomIn(transform.position);
+		Vector3 position = (_actor.transform.position + transform.position) / 2;
+
+		CameraSystem.CameraTarget.Instance.ZoomIn(position);
 
 		DialogueHandler.Instance.OnDialogueEnd += OnDialogueEnded;
 		DialogueHandler.Instance.Initialize(DialogueData);
