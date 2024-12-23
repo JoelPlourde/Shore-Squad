525f09f704735bd8a21d2c4a456c9387ec93821e
JoelPlourde <32371333+JoelPlourde@users.noreply.github.com>
2024-12-23 08:47:34 -0500

    Add Processing Task (with animation) (#12)
    * Add Processing Task (with animation)
    * Bump the TaskPriority of the Processing task
    
    ---------
    
    Co-authored-by: joelplourde4 <jplourde@cloudops.com>

--- Assets/Scripts/Inventory/ItemEffects/Effects/Process.cs
+++ Assets/Scripts/Inventory/ItemEffects/Effects/Process.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using UI;
 using CraftingSystem;
+using TaskSystem;
 
 namespace ItemSystem {
 	namespace EffectSystem {
@@ -9,6 +10,7 @@ namespace ItemSystem {
 
             private Actor _actor;
             private Item _item;
+            private RecipeData _recipeData;
 
 			public void Activate(Actor actor, Item item, ItemEffect itemEffect) {
                 if (ReferenceEquals(itemEffect.RecipeDatas, null)) {
@@ -19,30 +21,46 @@ namespace ItemSystem {
                 _actor = actor;
                 _item = item;
                 
-                RecipeHandler.Instance.Show(itemEffect.RecipeDatas, OnRecipeClicked, item.ItemData.Sprite);
+                RecipeHandler.Instance.Show(itemEffect.RecipeDatas, StartProcessingTask, item.ItemData.Sprite);
 			}
 
-            public void OnRecipeClicked(RecipeData recipeData) {
-                Item[] itemsArray = _actor.Inventory.GetItems().ToArray();
+            public void StartProcessingTask(RecipeData recipeData) {
+                _recipeData = recipeData;
 
-                if (!recipeData.CheckIfRecipeIsValid(itemsArray)) {
+                if (!CheckIfRecipeIsValid()) {
+                    Debug.LogWarning("This recipe is not valid.");
+                    return;
+                }
+
+                ProcessArguments processArguments = new ProcessArguments(recipeData.RequiredProgress, OnProcessingDone);
+
+                _actor.TaskScheduler.CreateTask(processArguments, TaskPriority.HIGH);
+            }
+
+            public void OnProcessingDone() {
+                if (!CheckIfRecipeIsValid()) {
                     Debug.LogWarning("This recipe is not valid.");
                     return;
                 }
 
                 _actor.Emotion.PlayEmote(EmoteSystem.EmoteType.PROCESS);
 
-                _actor.Inventory.RemoveItemsFromInventory(recipeData.Inputs, out List<Item> removedItems);
+                _actor.Inventory.RemoveItemsFromInventory(_recipeData.Inputs, out List<Item> removedItems);
                 if (removedItems.Count > 0) {
                     Debug.Log("We could not remove the item!!");
                     return;
                 }
 
-                _actor.Inventory.AddItemsToInventory(recipeData.Outputs, out List<Item> addedItems);
+                _actor.Inventory.AddItemsToInventory(_recipeData.Outputs, out List<Item> addedItems);
                 if (addedItems.Count > 0) {
                     Debug.Log("We could not add the item!!");
                     return;
                 }
+            }
+
+            private bool CheckIfRecipeIsValid() {
+                Item[] itemsArray = _actor.Inventory.GetItems().ToArray();
+                return _recipeData.CheckIfRecipeIsValid(itemsArray);
             }
 		}
 	}
