ff6f29be700995335b50aafac3d0591338a44c1e
joelplourde4 <jplourde@cloudops.com>
2025-01-03 15:51:31 -0500

    Dynamically add onclick events to the Settings menu

--- Assets/Scripts/UI/SettingsHandler.cs
+++ Assets/Scripts/UI/SettingsHandler.cs
@@ -11,6 +11,21 @@ namespace UI {
 		private void Awake() {
 			Canvas = GetComponent<Canvas>();
 			Instance = this;
+
+			Button saveButton = transform.Find("SaveButton").GetComponent<Button>();
+			saveButton.onClick.AddListener(() => {
+				SaveManager.Instance.SaveGame();
+			});
+
+			Button loadButton = transform.Find("LoadButton").GetComponent<Button>();
+			loadButton.onClick.AddListener(() => {
+				throw new UnityException("Not yet implemented!");
+			});
+
+			Button exitButton = transform.Find("ExitButton").GetComponent<Button>();
+			exitButton.onClick.AddListener(() => {
+				Application.Quit();
+			});
 		}
 
 		public void Open(Actor actor) {
